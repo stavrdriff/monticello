dcdf420aac1ae9be61c1fbd70d9cc43c29bcbf76
stavrdriff <stavr.driff@gmail.com>
2021-06-22 11:54:42 +0300

    owl carousel

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,26 @@ $(document).ready(function () {
       loop: true,
    });
 });
+$(document).ready(function () {
+   $('.owl-carousel').owlCarousel({
+      loop: true,
+      dots: true,
+      margin: 10,
+      responsiveClass: true,
+      responsive: {
+         0: {
+            items: 1,
+            nav: true
+         },
+         600: {
+            items: 3,
+            nav: false
+         },
+         1000: {
+            items: 3,
+            nav: true,
+
+         }
+      }
+   })
+});
